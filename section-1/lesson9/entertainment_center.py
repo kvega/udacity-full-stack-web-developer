1166dd6fb539be542647f71c51b71bafabe00715
kvega <kevin.n.vega@gmail.com>
2018-03-15 10:30:35 -0700

    finished lesson 9

--- section-1/lesson9/entertainment_center.py
+++ section-1/lesson9/entertainment_center.py
@@ -1,8 +1,20 @@
 import media
-
+import fresh_tomatoes
 toy_story = media.Movie("Toy Story",
 						"A story of a boy and his toys that come to life",
-						"https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg",
+						"https://goo.gl/images/tnnRAf",
 						"https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-print(toy_story.storyline)
+avatar = media.Movie("Avatar",
+					 "A marine on an alien planet",
+					 "https://t0.gstatic.com/images?q=tbn:ANd9GcQCfmvrE4fMo2cd8esc7mDZPtFSJThAujddMPkRtti1_ij6u-jp",
+					 "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+empire_strikes_back = media.Movie("Empire Strikes Back",
+								  "In an attempt to convert Luke to the dark side, Darth Vader lures young Skywalker into a trap in the Cloud City",
+								  "https://t1.gstatic.com/images?q=tbn:ANd9GcTtXwQAEDxEY3E9Nn78H96VZCjlV6hZWPlDd5IpVNyeuzO2vT17",
+								  "https://www.youtube.com/watch?v=JNwNXF9Y6kY")
+
+
+movies = [toy_story, avatar, empire_strikes_back]
+fresh_tomatoes.open_movies_page(movies)
